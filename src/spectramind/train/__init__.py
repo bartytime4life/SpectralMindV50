ec5ca38182ae16f97cf05ae5838b8a27a7ba1a61
bartytime4life <blackbart555@gmail.com>
2025-09-09 08:50:40 -0500

    Update __init__.py

--- src/spectramind/train/__init__.py
+++ src/spectramind/train/__init__.py
@@ -1,1 +1,11 @@
 
+from .trainer import train_from_config
+from .registry import get_model_builder, get_loss_builder, get_optimizer_builder, get_scheduler_builder
+
+__all__ = [
+    "train_from_config",
+    "get_model_builder",
+    "get_loss_builder",
+    "get_optimizer_builder",
+    "get_scheduler_builder",
+]
